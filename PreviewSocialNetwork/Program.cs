0c3d3fb017cd488e98e9d59f272f98585e2d1fdd
Amind <46020705+Amindlog@users.noreply.github.com>
2021-04-03 15:43:42 +0400

    Добавьте файлы проекта.

---
+++ PreviewSocialNetwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PreviewSocialNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
