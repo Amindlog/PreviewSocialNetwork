066e2ed2f3b7517aae31f3204bc434e5af610dbf
Jenausmax <mminmax@gmail.com>
2021-04-04 00:43:19 +0300

    Создание HttpClient. Попытка написания запросов.

--- PreviewSocialNetwork.App/Services/TelegramService.cs
+++ PreviewSocialNetwork.App/Services/TelegramService.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using PreviewSocialNetwork.Domain.Interfaces;
 using PreviewSocialNetwork.Domain.Models;
@@ -10,6 +12,15 @@ namespace PreviewSocialNetwork.App.Services
 {
     public class TelegramService : IServiceSocialNetwork, IAuth
     {
+        private string _botToken = "718470687:AAF-SsRrPbXWoPyHLo8lIN7aHowpGzjg-Go";
+        private int chat_id = 0;
+        private HttpClient client;
+
+        public TelegramService()
+        {
+            client = new HttpClient();
+        }
+
         public bool SendSocialNetwork(IMessagePreview message)
         {
             throw new NotImplementedException();
@@ -19,5 +30,32 @@ namespace PreviewSocialNetwork.App.Services
         {
             throw new NotImplementedException();
         }
+
+        public async Task SendMessage (HttpClient client, IMessagePreview message)
+        {
+            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/sendMessage?{chat_id}5&text={message.TimeMessage}{message.MessageText}");
+            Console.WriteLine(response.ToString());
+
+        }
+
+        public async Task GetUpdate(HttpClient client)
+        {
+            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/getUpdates");
+            //var deser = JsonSerializer.Deserialize<Chat>(response);
+        }
+    }
+
+    //"chat": {
+    //"id": -1001370618615,
+    //"title": "Tes",
+    //"username": "Tewaaaaa",
+    //"type": "channel"
+
+    public class Chat
+    {
+        public int id;
+        public string title;
+        public string username;
+        public string type;
     }
 }
