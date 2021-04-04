6a66308f3e5e3a182b34a58f037c3cc3ca3bc167
Alexander <trulander@mail.ru>
2021-04-04 16:52:02 +0500

    почистил код.

--- PreviewSocialNetwork.App/Logic/LogicServiceExpILogicServices.cs
+++ PreviewSocialNetwork.App/Logic/LogicServiceExpILogicServices.cs
@@ -63,19 +63,5 @@ namespace PreviewSocialNetwork.App.Services
 
 
         }
-        /*
-        public IServiceSocialNetwork Service { get; }
-
-        public List<int> ParserStringGetIssue(string message)//1 2 3
-        {
-            List<int> list = new List<int>();
-            string[] words = message.Split(' ');
-            foreach (var item in words)
-            {
-                list.Add(Convert.ToInt32(item));
-            }
-            return list;
-        }
-        */
     }
 }
