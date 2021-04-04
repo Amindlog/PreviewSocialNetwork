ba4e2efd422e6fd9903c6db91591cbb813a2b16e
Jenausmax <mminmax@gmail.com>
2021-04-04 13:14:38 +0300

    Попытка реализации отправки в телеграм.

--- PreviewSocialNetwork.TelegramService/TelegramService.cs
+++ PreviewSocialNetwork.TelegramService/TelegramService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -10,52 +11,42 @@ using PreviewSocialNetwork.Domain.Models;
 
 namespace PreviewSocialNetwork.App.Services
 {
-    public class TelegramService : IServiceSocialNetwork, IAuth
+    public class TelegramService : IServiceSocialNetwork
     {
         private string _botToken = "718470687:AAF-SsRrPbXWoPyHLo8lIN7aHowpGzjg-Go";
-        private int chat_id = 0;
-        private HttpClient client;
+        private int _chat_id = 0;
+        public HttpClient Client { get; set; }
 
         public TelegramService()
         {
-            client = new HttpClient();
+            Client = new HttpClient();
+            GetUpdate(Client);
         }
 
         public bool SendSocialNetwork(IMessagePreview message)
         {
-            throw new NotImplementedException();
+            SendMessage(Client, message);
+            return true;
         }
 
-        public bool LoginAuthorization(User user)
-        {
-            throw new NotImplementedException();
-        }
 
-        public async Task SendMessage (HttpClient client, IMessagePreview message)
+        public async void SendMessage (HttpClient client, IMessagePreview message)
         {
-            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/sendMessage?{chat_id}5&text={message.TimeMessage}{message.MessageText}");
-            Console.WriteLine(response.ToString());
+            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/sendMessage?{_chat_id}5&text={message.TimeMessage}{message.MessageText}");
+
+            Console.WriteLine($"Ответ от Telegram: " + response.StatusCode);
+        
 
         }
 
         public async Task GetUpdate(HttpClient client)
         {
-            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/getUpdates");
-            //var deser = JsonSerializer.Deserialize<Chat>(response);
+            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/getUpdates?limit=1");
+            var res = response.Content.ReadAsStringAsync().Result;
+            var stringResultSplit = res.Substring(95).Split(',');
+            int.TryParse(stringResultSplit[0], out _chat_id);
         }
     }
 
-    //"chat": {
-    //"id": -1001370618615,
-    //"title": "Tes",
-    //"username": "Tewaaaaa",
-    //"type": "channel"
-
-    public class Chat
-    {
-        public int id;
-        public string title;
-        public string username;
-        public string type;
-    }
+    
 }
