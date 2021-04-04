e8d2954ec0790bb2b4bb4802e8f0f1499906f571
Jenausmax <mminmax@gmail.com>
2021-04-04 14:37:30 +0300

    Поправлен баг с URL

--- PreviewSocialNetwork.TelegramService/TelegramService.cs
+++ PreviewSocialNetwork.TelegramService/TelegramService.cs
@@ -14,7 +14,7 @@ namespace PreviewSocialNetwork.App.Services
     public class TelegramService : IServiceSocialNetwork
     {
         private string _botToken = "718470687:AAF-SsRrPbXWoPyHLo8lIN7aHowpGzjg-Go";
-        private int _chat_id = 0;
+        private static long _chat_id;
         public HttpClient Client { get; set; }
 
         public TelegramService()
@@ -32,7 +32,8 @@ namespace PreviewSocialNetwork.App.Services
 
         public async void SendMessage (HttpClient client, IMessagePreview message)
         {
-            var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/sendMessage?{_chat_id}5&text={message.TimeMessage}{message.MessageText}");
+            var url = $"https://api.telegram.org/bot{_botToken}/sendMessage?chat_id={_chat_id}&text={message.TimeMessage}{message.MessageText}";
+            var response = await client.GetAsync(url);
 
             Console.WriteLine($"Ответ от Telegram: " + response.StatusCode);
         
@@ -44,7 +45,7 @@ namespace PreviewSocialNetwork.App.Services
             var response = await client.GetAsync($"https://api.telegram.org/bot{_botToken}/getUpdates?limit=1");
             var res = response.Content.ReadAsStringAsync().Result;
             var stringResultSplit = res.Substring(95).Split(',');
-            int.TryParse(stringResultSplit[0], out _chat_id);
+            _chat_id = Convert.ToInt64(stringResultSplit[0]);
         }
     }
 
